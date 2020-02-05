452767fa31f515f5d16ed6823f78b9808a8063d1
CISCTBD <43619934+CISCTBD@users.noreply.github.com>
2020-02-05 08:33:43 -0500

    Opening a web link in a new blank page

--- StackOverflowProject/js/custom.js
+++ StackOverflowProject/js/custom.js
@@ -2,6 +2,10 @@
 $(document).ready(function(){
     
     $("#result").on("click","a",function(e){
+        if ($(this).attr("class")!="q-title"){
+            window.open(this.attr("href"),'_blank')
+            return;
+        }
         e.preventDefault();
         var myClass=$(this).attr("id");
         $(".dis-content ."+myClass).toggle('slow');
@@ -11,7 +15,7 @@ $(document).ready(function(){
 
     $("button").click(function(){
         var numberOfResult = 0;
-        var sort = "";
+        var sort;
         var resultNumberField = document.getElementById("result-number");
         var input = document.getElementById("tag");
         var fromdate = Math.round(new Date().getTime()/1000)-7*86400;
@@ -26,8 +30,8 @@ $(document).ready(function(){
 
         for(var i = 0; i < radioButtons.length; i ++) {
             if (radioButtons[i].checked) {
-                sort = radioButtons[i].value;
-                targeturl += sort;
+                sort = radioButtons[i];
+                targeturl += sort.value;
             }
         }
         targeturl += '&tagged='+input.value+'&site=stackoverflow&filter=!iCF4LoRm6FXfS9mZ15yq(1';
@@ -45,7 +49,7 @@ $(document).ready(function(){
                 });
                 
             });
-            resultNumberField.innerHTML = "Result: "+numberOfResult+" - tag: "+input.value+", sort: "+ sort;
+            resultNumberField.innerHTML = "Result: "+numberOfResult+" - tag: "+input.value+",  sort: "+ sort.value;
         });  
        
         
@@ -56,9 +60,10 @@ function addElement(object, id){
     var htmlResult = "";
    
     htmlResult += '<div class="dis-question">';
-    htmlResult += '<div class="dis-title"><a href="#" id='+id+'><h4>'+object.title+'</h4></a></div>';
+    htmlResult += '<div class="dis-title"><a href="#" id='+id+' class="q-title"><h4>'+object.title+'</h4></a></div>';
     htmlResult += '<div class="dis-content"><div class='+id+' ">';
     htmlResult += object.body;
+    htmlResult += '<br><p><b> Original Website: <a href="'+object.link+'" target="_blank">'+object.link+'</a></b></p>';
     htmlResult += '</div></div></div>';
     document.getElementById("result").innerHTML += htmlResult;
 
